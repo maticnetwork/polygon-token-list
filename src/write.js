9f8d72ca26287e7f90705232498b7c0a73c797c1
Rajesh Chaganti <40339800+py-zoid@users.noreply.github.com>
2022-02-21 12:38:35 +0530

    Deploy staging list

--- src/write.js
+++ src/write.js
@@ -4,6 +4,7 @@ const listRegistry = require('./listRegistry.json')
 
 const tokenlists = buildList()
 
+// All Tokens Tokenlist
 fs.writeFile('build/tokenlists/allTokens.tokenlist.json', JSON.stringify(tokenlists.allTokensTokenList, null, 2), (err) => {
   if (err) {
     throw err
@@ -11,6 +12,7 @@ fs.writeFile('build/tokenlists/allTokens.tokenlist.json', JSON.stringify(tokenli
   console.log('allTokens.tokenlist.json successfully built')
 })
 
+// Default Tokenlist
 fs.writeFile('build/tokenlists/default.tokenlist.json', JSON.stringify(tokenlists.defaultTokenList, null, 2), (err) => {
   if (err) {
     throw err
@@ -18,6 +20,7 @@ fs.writeFile('build/tokenlists/default.tokenlist.json', JSON.stringify(tokenlist
   console.log('default.tokenlist.json successfully built')
 })
 
+// Testnet Tokenlist
 fs.writeFile('build/tokenlists/testnet.tokenlist.json', JSON.stringify(tokenlists.testnetTokenList, null, 2), (err) => {
   if (err) {
     throw err
@@ -25,6 +28,7 @@ fs.writeFile('build/tokenlists/testnet.tokenlist.json', JSON.stringify(tokenlist
   console.log('testnet.tokenlist.json successfully built')
 })
 
+// Cross-Chain Tokenlist
 fs.writeFile('build/tokenlists/cross-chain.tokenlist.json', JSON.stringify(tokenlists.ccTokensList, null, 2), (err) => {
   if (err) {
     throw err
@@ -32,6 +36,15 @@ fs.writeFile('build/tokenlists/cross-chain.tokenlist.json', JSON.stringify(token
   console.log('cross-chain.tokenlist.json successfully built')
 })
 
+// Staging Tokenlist
+fs.writeFile('build/tokenlists/staging.tokenlist.json', JSON.stringify(tokenlists.stagingTokenList, null, 2), (err) => {
+  if (err) {
+    throw err
+  }
+  console.log('staging.tokenlist.json successfully built')
+})
+
+// List Registery
 fs.writeFile('build/listRegistry.json', JSON.stringify(listRegistry, null, 2), (err) => {
   if (err) {
     throw err
