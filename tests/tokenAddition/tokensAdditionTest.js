0a86c6b38414cd80837d110f29c2b933605c59cd
R Chaganti <rajesh.chaganti@polygon.technology>
2024-09-20 19:08:04 +0400

    remove unused tokenlists

--- tests/tokenAddition/tokensAdditionTest.js
+++ tests/tokenAddition/tokensAdditionTest.js
@@ -5,105 +5,15 @@ const lists = require("../../src/listRegistry.json");
 describe("Test new token additions", () => {
     const tokenlists = buildList();
 
-    it("contains no duplicate child addresses", () => {
+    it("contains no duplicate origin token addresses", () => {
         let map = {};
-        for (const token of tokenlists.polygonTokensTokenList.tokens) {
-            const key = `${token.address.toLowerCase()}`;
-            expect(typeof map[key]).to.equal(
+        for (const token of tokenlists.mappedTokenList.tokens) {
+            const key = `${token.originTokenAddress.toLowerCase()}`;
+            expect(typeof map[token.originNetworkId][key]).to.equal(
                 "undefined",
-                `duplicate child address: ${token.address} - polygonTokens list`
+                `duplicate originTokenAddress: ${token.originTokenAddress} in mappedTokenList`
             );
-            map[key] = true;
-        }
-        map = {};
-        for (const token of tokenlists.popularTokenList.tokens) {
-            const key = `${token.address.toLowerCase()}`;
-            expect(typeof map[key]).to.equal(
-                "undefined",
-                `duplicate child address: ${token.address} - polpularTokens list`
-            );
-            map[key] = true;
-        }
-        map = {};
-        for (const token of tokenlists.testnetTokenList.tokens) {
-            const key = `${token.address.toLowerCase()}`;
-            expect(typeof map[key]).to.equal(
-                "undefined",
-                `duplicate child address: ${token.address} - testnet list`
-            );
-            map[key] = true;
-        }
-        map = {};
-        for (const token of tokenlists.blacklistTokenList.tokens) {
-            const key = `${token.address.toLowerCase()}`;
-            expect(typeof map[key]).to.equal(
-                "undefined",
-                `duplicate child address: ${token.address} - blacklist token list`
-            );
-            map[key] = true;
-        }
-    });
-
-    it("contains no duplicate root addresses", () => {
-        let map = { pos: {}, plasma: {} };
-        for (const token of tokenlists.polygonTokensTokenList.tokens) {
-            if (
-                token.extensions.rootAddress.toLowerCase() !=
-                "0x0000000000000000000000000000000000000000"
-            ) {
-                const tag = token.tags.includes("pos") ? "pos" : "plasma";
-                const key = `${token.extensions.rootAddress.toLowerCase()}`;
-                expect(typeof map[tag][key]).to.equal(
-                    "undefined",
-                    `duplicate root address: ${token.extensions.rootAddress} - polygonTokens list`
-                );
-                map[tag][key] = true;
-            }
-        }
-        map = { pos: {}, plasma: {} };
-        for (const token of tokenlists.popularTokenList.tokens) {
-            if (
-                token.extensions.rootAddress.toLowerCase() !=
-                "0x0000000000000000000000000000000000000000"
-            ) {
-                const tag = token.tags.includes("pos") ? "pos" : "plasma";
-                const key = `${token.extensions.rootAddress.toLowerCase()}`;
-                expect(typeof map[tag][key]).to.equal(
-                    "undefined",
-                    `duplicate root address: ${token.extensions.rootAddress} - polpularTokens list`
-                );
-                map[tag][key] = true;
-            }
-        }
-        map = { pos: {}, plasma: {} };
-        for (const token of tokenlists.testnetTokenList.tokens) {
-            if (
-                token.extensions.rootAddress.toLowerCase() !=
-                "0x0000000000000000000000000000000000000000"
-            ) {
-                const tag = token.tags.includes("pos") ? "pos" : "plasma";
-                const key = `${token.extensions.rootAddress.toLowerCase()}`;
-                expect(typeof map[tag][key]).to.equal(
-                    "undefined",
-                    `duplicate root address: ${token.extensions.rootAddress} - testnet list`
-                );
-                map[tag][key] = true;
-            }
-        }
-        map = { pos: {}, plasma: {} };
-        for (const token of tokenlists.blacklistTokenList.tokens) {
-            if (
-                token.extensions.rootAddress.toLowerCase() !=
-                "0x0000000000000000000000000000000000000000"
-            ) {
-                const tag = token.tags.includes("pos") ? "pos" : "plasma";
-                const key = `${token.extensions.rootAddress.toLowerCase()}`;
-                expect(typeof map[tag][key]).to.equal(
-                    "undefined",
-                    `duplicate root address: ${token.extensions.rootAddress} - blacklist token list`
-                );
-                map[tag][key] = true;
-            }
+            map[token.originNetworkId][key] = true;
         }
     });
 
